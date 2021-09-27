313dbd4eb4e4c23b03fea021a7ab2242de3083a2
Reductito <damianbell96@gmail.com>
2021-09-27 15:04:06 +0100

    added selected db fields rather than using * and wrapped text in <p> paragraphs, also added normalize and css files while waiting

--- index.php
+++ index.php
@@ -1,24 +1,21 @@
 <?php
 $db = new PDO('mysql:host=db; dbname=damian-collection', 'root', 'password');
 $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-$query = $db->prepare("SELECT * FROM `albums`");
+$query = $db->prepare("SELECT `name`, `artist`, `release-year`, `genre`, `record-label` FROM `albums`");
 $query->execute();
 $albums = $query->fetchAll();
 
 $displayalbums = '';
 
 foreach ($albums as $album) {
-    $displayalbums .= '<div class="album" style="padding: 10px;"> This album is called ' . $album['name'] . '<br>' .
-                      'The creator is ' . $album['artist'] . '<br>' .
-                      'The release year is ' . $album['release-year'] . '<br>' .
-                      'The genre is ' . $album['genre'] . '<br>' .
-                      'Label - ' . $album['record-label'] . '</div>';
+    $displayalbums .= '<div class="album" style="padding: 10px;"> <p>This album is called ' . $album['name'] . '</p>' .
+                      '<p>The creator is ' . $album['artist'] . '</p>' .
+                      '<p>The release year is ' . $album['release-year'] . '</p>' .
+                      '<p>The genre is ' . $album['genre'] . '</p>' .
+                      '<p>Record label - ' . $album['record-label'] . '</p></div>';
 }
 
 echo $displayalbums;
 
-//
-//echo '<pre>';
-//echo var_dump($albums);
-//echo '</pre>';
+?>
 
